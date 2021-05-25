d295dd9434078ffb1dd64586ca0510d0b53c9535
Quyen Luu <luqu0501@gmail.com>
2021-05-25 09:51:33 +0700

    feat: add 2 sum descriptions

--- 1. Two-sum.js
+++ 1. Two-sum.js
@@ -2,9 +2,14 @@
  * Given an array of integers nums and an integer target, 
  * Return indices of the two numbers such that they add up to target
  *
+ * Examples:
+ * Input: nums = [2, 7, 11, 15], target = 9
+ * Output: [0, 1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
  * @params {Array} nums
  * @params {Number} target
- * @return {Array}
+ * @return {Array} indices of the two numbers
  */
 const twoSum = (nums, target) => {
     let dataSet = new Map();
